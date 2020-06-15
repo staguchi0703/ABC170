0b3f5d2e9de93930b75ce21809999642bbc43ff6
sho Taguchi <s.taguchi0703@gmail.com>
2020-06-15 22:08:19 +0900

    DをAC

--- D/resolve.py
+++ D/resolve.py
@@ -2,25 +2,23 @@ def resolve():
     '''
     code here
     '''
-    import collections
+
     N = int(input())
     A_list = [int(item) for item in input().split()]
-
     A_list.sort()
-    cnt = 0
 
-    if A_list:
-        dp = [True for item in range(A_list[-1]+1)]
-        for item in A_list:
-            if dp[item]: 
-                temp = item
-                while temp <= A_list[-1]:
-                    temp += item
-                    dp[temp] = False
 
+    dp = [0 for item in range(A_list[-1]+1)]
     for item in A_list:
-        if dp[item]:
-            # dp[item]が複数か単数かの判断を入れたら行けるはず
+
+        temp = item
+        while temp <= A_list[-1]:
+            dp[temp] += 1
+            temp += item
+
+    cnt = 0
+    for item in A_list:
+        if dp[item] == 1:
             cnt += 1
     print(cnt)
 
